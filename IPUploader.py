da9e033f7eaabde3bb7888dcf3bd7de86d813fa8
Tanmay Thakur rpi <tanmaythakur2@gmail.com>
2018-03-25 19:14:45 -0500

    Add execution step info

--- IPUploader.py
+++ IPUploader.py
@@ -6,6 +6,7 @@ from time import strftime, gmtime
 
 GIT_BRANCH_NAME = "master"
 
+
 def _get_old_ip():
     with open('current_IP', 'r') as fp:
         old_ip = fp.read().strip()
@@ -13,12 +14,15 @@ def _get_old_ip():
 
 
 def _save_new_ip(ip):
+    print "Writing th IP to the file....."
     with open('current_IP', 'w') as fp:
         fp.write(ip)
+    print "\rWriting th IP to the file.....Done\n\n"
 
 
 def _upload_on_git(ip):
     try:
+        print " Uploading new IP...\n"
         # confirm the correct branch
         os.system("git checkout " + GIT_BRANCH_NAME)
 
@@ -26,17 +30,20 @@ def _upload_on_git(ip):
         os.system("git add -A")
         os.system("git commit -m \"Recorded new ip " + str(ip) + "\" ")
         os.system("git push origin " + GIT_BRANCH_NAME)
+        print "\rUploading new IP...Done\n\n"
     except Exception, ex:
-        print ex
+        print "\rUploading new IP..." + ex
 
 
 if __name__ == '__main__':
     try:
+        print "Program execution started...\n\n"
         ip = requests.get('http://httpbin.org/ip').json()['origin']
         old_ip = _get_old_ip()
         if old_ip != ip:
+            print "Ip changed...\n"
             _save_new_ip(ip)
             _upload_on_git(ip)
+        print "Program executed successfully....\n\n"
     except Exception, ex:
-        msg = '%s - Error: %s' % (strftime("%m-%d-%Y %H:%M:%S", gmtime()), str(ex))
-        print msg
+        print "Program execution failed...." + ex
