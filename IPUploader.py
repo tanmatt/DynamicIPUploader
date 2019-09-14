04c5596355d044a0cb98278309aad0b5422f2638
Tanmay Thakur <tanmaythakur2@gmail.com>
2019-09-14 14:22:29 -0500

    Recorded new ip 71.41.191.100, 71.41.191.100

--- IPUploader.py
+++ IPUploader.py
@@ -1,8 +1,7 @@
 ___author__ = 'tanmaythakur.com'
 
-import requests, os
-from time import strftime, gmtime
-
+import requests
+import os
 
 GIT_BRANCH_NAME = "master"
 
@@ -14,15 +13,15 @@ def _get_old_ip():
 
 
 def _save_new_ip(ip):
-    print "Writing th IP to the file....."
+    print("Writing th IP to the file.....")
     with open('current_IP', 'w') as fp:
         fp.write(ip)
-    print "\rWriting th IP to the file.....Done\n\n"
+    print("\rWriting th IP to the file.....Done\n\n")
 
 
 def _upload_on_git(ip):
     try:
-        print " Uploading new IP...\n"
+        print(" Uploading new IP...\n")
         # confirm the correct branch
         os.system("git checkout " + GIT_BRANCH_NAME)
 
@@ -30,20 +29,20 @@ def _upload_on_git(ip):
         os.system("git add -A")
         os.system("git commit -m \"Recorded new ip " + str(ip) + "\" ")
         os.system("git push origin " + GIT_BRANCH_NAME)
-        print "\rUploading new IP...Done\n\n"
-    except Exception, ex:
-        print "\rUploading new IP..." + ex
+        print("\rUploading new IP...Done\n\n")
+    except Exception as ex:
+        print("\rUploading new IP..." + ex)
 
 
 if __name__ == '__main__':
     try:
-        print "Program execution started...\n\n"
+        print("Program execution started...\n\n")
         ip = requests.get('http://httpbin.org/ip').json()['origin']
         old_ip = _get_old_ip()
         if old_ip != ip:
-            print "Ip changed...\n"
+            print("Ip changed...\n")
             _save_new_ip(ip)
             _upload_on_git(ip)
-        print "Program executed successfully....\n\n"
-    except Exception, ex:
-        print "Program execution failed...." + ex
+        print("Program executed successfully....\n\n")
+    except Exception as ex:
+        print("Program execution failed...." + ex)
